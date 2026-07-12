1fb0d7bdfb77584df8190ae0f4e3bc8c65841b75
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2026-07-12 18:26:41 -0300

    creacion de minial apis com services

--- curso/src/MasterNet.RatingService/Program.cs
+++ curso/src/MasterNet.RatingService/Program.cs
@@ -14,6 +14,29 @@ var app = builder.Build();
 
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
+app.MapPost("/ratings", (RatingStore store, RatingRequest request) =>
+{
+    if (string.IsNullOrWhiteSpace(request.Id))
+        return Results.BadRequest("Id is required");
+
+    store.AddRating(request.Id, request.Rating);
+    return Results.Ok();
+});
+
+app.MapGet("/ratings/{id}", (RatingStore store, string id) =>
+{
+    if (string.IsNullOrWhiteSpace(id))
+        return Results.BadRequest("Id is required");
+
+    var rating = store.GetAverangeRating(id);
+    if (rating == null)
+        return Results.NotFound();
+
+    return Results.Ok(rating);
+});
+
 app.MapDefaultEndpoints();
 
 app.Run();
+
+public record RatingRequest(string Id, int Rating);
