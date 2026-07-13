4c206d484b6ec34a2a23de02686af857a98244ff
ReinanGuilhermeWork <reinanguilherme.work@gmail.com>
2026-07-13 08:21:18 -0300

    conectando service en aspire

--- curso/src/MasterNet.AppHost/AppHost.cs
+++ curso/src/MasterNet.AppHost/AppHost.cs
@@ -8,16 +8,25 @@ var myParameter = builder.AddParameter("MyParameter");
 
 var myConnectionString = builder.AddConnectionString("MyConnectionString");
 
-var server = builder.AddSqlServer("server", password, 1433).WithLifetime(ContainerLifetime.Persistent);
+var server = builder.AddSqlServer("server", password, 1433)
+    .WithLifetime(ContainerLifetime.Persistent);
 
 var db = server.AddDatabase("MasterNetDB");
+
+var cache = builder.AddRedis("cache")
+    .WithRedisCommander()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var ratingService = builder.AddProject<MasterNet_RatingService>("rating-service")
+    .WithReference(cache)
+    .WaitFor(cache);
 
 var api = builder.AddProject<MasterNet_WebApi>("api")
     .WithHttpEndpoint(5001)
     .WithReference(db)
+    .WithReference(ratingService)
     .WaitFor(db)
-    .WithEnvironment("MY_ENV_PARAMETER", myParameter)
-    .WithReference(myConnectionString);
+    .WaitFor(ratingService);
 
 var client = builder.AddProject<MasterNet_Client>("client")
     .WithReference(api)
@@ -28,7 +37,5 @@ builder.AddProject<MasterNet_MigrationService>("migration")
     .WithReference(db)
     .WaitFor(db)
     .WithParentRelationship(server);
-
-builder.AddProject<Projects.MasterNet_RatingService>("masternet-ratingservice");
 
 builder.Build().Run();
